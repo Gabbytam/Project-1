d41b5bde1538570385a6b92cc622b7d46fa1aa78
Gabbytam <gtam2@fordham.edu>
2020-09-24 14:04:33 -0700

    made and base line styled a board that is 11x14 squares

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log('here we goooooo')
+console.log('here we goooooo');
+
+document.addEventListener('DOMContentLoaded', ()=> {
+    let boardRows;
+    let boardSquares;
+    for(let i= 0; i<11; i++){
+        boardRows= document.createElement('div');
+        boardRows.classList.add('row');
+        container.append(boardRows);
+        for(let j=0; j<14; j++){
+            boardSquares= document.createElement('div');
+            boardSquares.classList.add('square');
+            boardRows.append(boardSquares);
+        }
+    }
+})
